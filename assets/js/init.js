816e978a5c593307c76120d672f549c19021630f
Jérémie Noël <dev.jnoel@gmail.com>
2019-02-22 21:47:40 +0100

    init js, wip

--- assets/js/init.js
+++ assets/js/init.js
@@ -1,3 +1,8 @@
 $(document).ready(function() {
     M.updateTextFields();
-  });
+});
+import * as templating from '../html/templating.js';
+const root = document.getElementById("root");
+
+
+root.innerHTML = templating.rootFile;
